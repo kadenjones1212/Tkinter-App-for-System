795ccfff86dc210cb6f764fccaeac9c7c25153cb
kadenjones1212 <kadenjones1212@gmail.com>
2025-02-26 15:24:35 -0500

    More fixing.
    stuff

--- backend.py
+++ backend.py
@@ -99,7 +99,7 @@ def getToday():
      revisedDate = datetime.now().strftime("%c")
      return revisedDate
 def getShortToday():
-    revisedDate = datetime.now().strftime("%d/%m/%y\n%H:%M")
+    revisedDate = datetime.now().strftime("%d/%m/%y\n%I:%M %p")
     return revisedDate
 
             
