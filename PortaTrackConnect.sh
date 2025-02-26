8e75f30624426e43dc443376a88133160d7c7b4d
kadenjones1212 <kadenjones1212@gmail.com>
2025-02-26 14:37:34 -0500

    Stuff changed.

---
+++ PortaTrackConnect.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change directory to the folder where run.sh is located
+cd /home/kthej/Desktop/Tkinter-App-for-System/
+
+# Run the Python script frontend.py
+python3 frontend.py
--- systemVariables.py
+++ systemVariables.py
@@ -1,5 +1,7 @@
-scn_w = int(800*0.75)
-scn_h = int(480*0.75)
+import tkinter as tk
+root = tk.Tk()
+scn_w = root.winfo_screenwidth()
+scn_h = root.winfo_screenheight()
 center_x = int(scn_w/2)
 center_y = int(scn_h/2)
 mainButton_w = scn_w/1.25
