5760326470bbfcc256000924f0e41a69426d8a24
kadenjones1212 <kadenjones1212@gmail.com>
2025-03-07 14:21:02 -0500

    Stuff

--- bluetoothTest.py
+++ bluetoothTest.py
@@ -1,8 +1,8 @@
-import ubluetooth
-from ble_advertising import advertising_payload as adpl
+# #import ubluetooth
+# from ble_advertising import advertising_payload as adpl
 
-if __name__=='__main__':
-    ble = ubluetooth.BLE()
-    pl = adpl(name='TempSens', customData=56)  # 56 is some random "data" to test, put in whatever you like but there is a limit to the total advertisement length.
-    ble.active(True)
-    ble.gap_advertise(30000, adv_data=pl)
+# if __name__=='__main__':
+#     #ble = ubluetooth.BLE()
+#     pl = adpl(name='TempSens', customData=56)  # 56 is some random "data" to test, put in whatever you like but there is a limit to the total advertisement length.
+#     ble.active(True)
+#     ble.gap_advertise(30000, adv_data=pl)
